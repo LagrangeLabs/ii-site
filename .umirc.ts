6a09139bf562e6e3903d8bdf29c4ff7c8771ca80
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2021-01-12 20:56:24 +0800

    ✨feat: 修改umi配置

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   title: '实在智能',
   mode: 'site',
   logo: '/images/color-logo.png',
+  publicPath: '/ii-site/',
+  base: '/ii-site',
+  history: { type: 'hash' },
   // more config: https://d.umijs.org/config
   styles: [
     'https://cdn.bootcdn.net/ajax/libs/antd/4.9.3/antd.css',
